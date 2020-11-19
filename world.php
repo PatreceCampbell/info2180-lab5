d27fb7cff9d70b52d8ca72028b60f6f7ae2366d8
PatreceCampbell <patrece.campbell@mymona.uwi.edu>
2020-11-19 11:36:37 -0500

    Added Table to php file

--- world.php
+++ world.php
@@ -5,12 +5,12 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-//$country = $_GET['country'];
 
 if(isset($_GET['country'])){
   $country = $_GET['country'];
   $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+
 }
 if(!isset($_GET['country'])){
   $stmt = $conn->query("SELECT * FROM countries");
@@ -19,8 +19,17 @@ if(!isset($_GET['country'])){
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
 <ul>
+ <table>
+ <tr> 
+      <th>NAME</th>
+      <th>CONTINENT</th>
+      <th>INDEPENDENCE</th>
+      <th>HEAD OF STATE</th>
+  </tr> 
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <?php echo "<tr><td>". $row['name'] . "</td><td>". $row['continent'] . "</td><td>". $row['independence_year'] . "</td><td>". $row['head_of_state'] ."</td></tr>"; ?>
 <?php endforeach; ?>
+</table>
 </ul>
